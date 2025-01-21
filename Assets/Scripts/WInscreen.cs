1fe09688f4d68573be94d4c72dc67e0e3d0e5e30
DavidDiv23 <xxxbagudavidmihaixxx@gmail.com>
2025-01-21 13:06:38 +0100

    1306pm

---
+++ Assets/Scripts/WInscreen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WInscreen : MonoBehaviour
+{
+    public GameObject uiElement1;
+public GameObject uiElement2;
+public GameObject uiElement3;
+public GameObject uiElement4;
+
+
+private void OnTriggerEnter(Collider other)
+{
+    // Check if the player enters the trigger
+    if (other.CompareTag("Player"))
+    {
+        // Enable the UI element
+        uiElement1.SetActive(true);
+        uiElement2.SetActive(false);
+        uiElement3.SetActive(false);
+        uiElement4.SetActive(false);
+    }
+}
+
+private void OnTriggerExit(Collider other)
+{
+    // Check if the player exits the trigger
+    if (other.CompareTag("Player"))
+    {
+        // Disable the UI element
+        uiElement1.SetActive(false);
+        uiElement2.SetActive(true);
+        uiElement3.SetActive(true);
+        uiElement4.SetActive(false);
+    }
+}
+}
